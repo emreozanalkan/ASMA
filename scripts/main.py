ca0b7cb0dc3b87d5316c962357a83613da1cee52
mscv5 <mscv5@CO-ROBOT06.(none)>
2014-11-03 17:31:59 +0100

    Basic state machine implementaion to get a baisc idea

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,64 @@
 #!/usr/bin/python
 import welcome
-print 'Welcome'
-welcome.wel('welcome.txt')
+import roslib; roslib.load_manifest('asma')
+import rospy
+import smach
+import smach_ros
+
+# define state Foo
+class Welc(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+        self.isDone = 0
+
+    def execute(self, userdata):
+        rospy.sleep(1)
+        if self.isDone  == 0:
+            self.isDone  =  1
+            rospy.loginfo('Executing state Welcome')
+            welcome.wel('welcome.txt')
+            return 'outcome1'
+        else:
+            rospy.loginfo('Already executed state Welcome')
+            return 'outcome2'
+
+
+# define state Bar
+class StartMe(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome2'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state StartMe')
+        rospy.sleep(1)
+
+        return 'outcome2'
+
+
+# main
+def main():
+    rospy.init_node('asma')
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['STOP'])
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('StartMe', StartMe(),
+                               transitions={'outcome2':'Wel_come'})
+
+        smach.StateMachine.add('Wel_come', Welc(),
+                               transitions={'outcome1':'StartMe',
+                                            'outcome2':'STOP'})
+
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+
+
+if __name__ == '__main__':
+    main()
+
+#print 'Welcome'
+#welcome.wel('welcome.txt')
---
+++ scripts/welcome.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+import os
+import roslib
+roslib.load_manifest('sound_play')
+import rospy
+from sound_play.msg import SoundRequest
+from sound_play.libsoundplay import SoundClient
+
+def wel(filename):
+    #absolute path for this
+    fileloc = str( str(os.path.dirname(__file__)) +'/'+ filename)
+    f = open(fileloc, 'r')
+    #rospy.init_node('say', anonymous = True)
+    soundhandle = SoundClient()
+    rospy.sleep(1)
+    s = f.read()
+    print s
+    soundhandle.say(s,'voice_kal_diphone')
+    #may be loop with readline so there is a pause at every line.
+    rospy.sleep(1)
